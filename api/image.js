afa7a451ffb1c096f50760121135e5a2f2efd6ad
haitsam58 <haitsammasykur@gmail.com>
2026-05-24 03:01:08 +0700

    Update image.js

--- api/image.js
+++ api/image.js
@@ -4,17 +4,27 @@ const path = require("path");
 module.exports = (req, res) => {
     const referer = req.headers.referer || "";
 
-    const allowed = referer.includes(
-        "portfolio-haitsam.vercel.app"
+    console.log("REFERER:", referer);
+
+    // domain yang DIIZINKAN
+    const allowedDomains = [
+        "https://portfolio-haitsam.vercel.app",
+        "https://portofolio-haitsam-git-main-haitsam58s-projects.vercel.app"
+    ];
+
+    const allowed = allowedDomains.some(domain =>
+        referer.startsWith(domain)
     );
 
     if (!allowed) {
-        return res.status(403).send("Forbidden");
+        return res.status(403).send("Forbidden - Anti Leeching Active");
     }
 
     const imagePath = path.join(process.cwd(), "foto1.jpeg");
+
     const image = fs.readFileSync(imagePath);
 
     res.setHeader("Content-Type", "image/jpeg");
-    res.send(image);
+
+    return res.send(image);
 };
